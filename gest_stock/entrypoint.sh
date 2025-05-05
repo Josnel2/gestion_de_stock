83d2b06662c4fd2178184c7645a836cbb5b2bc76
joel0223 <fatisjules58@gmail.com>
2025-05-05 02:32:13 +0100

    configuration final

--- gest_stock/entrypoint.sh
+++ gest_stock/entrypoint.sh
@@ -1,14 +1,10 @@
-#!/script/bash
+#!/bin/bash
 
 set -e
-source /opt/env/scripts/activate
+source /env/bin/activate
 
 if [ "$1" = "gunicorn" ]; then
-
-exec gunicorn gest_stock.wsgi:application --bind 0.0.0.0:8000 --workers 3
+    exec gunicorn gest_stock.wsgi:application --bind 0.0.0.0:8000 --workers 3
 else
-
-python manage.py runserver 0.0.0.0:8000
-
+    python manage.py runserver 0.0.0.0:8000
 fi
-
